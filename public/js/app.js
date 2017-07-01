ce812aedd763251e676c4d54f86c185e1ea4f4b4
Renan Dias <renan@jera.com.br>
2017-07-01 16:23:38 -0400

    Refactoring code

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,31 @@
 var app = angular.module("app", ["ngRoute"]);
 
 app.controller("controller", function($scope, $location) {
-    $scope.login = function() {
-        $location.path("/home");
-    };
+  $scope.login = function() {
+    $location.path("/home");
+  };
 });
 
 app.config(function ($routeProvider) {
-    $routeProvider
-    .when("/notas-frequencias", {
-        templateUrl: "views/notas-frequencias.html",
-        controller: "controller"
-    })
-    .when("/login", {
-        templateUrl: "views/login.html",
-        controller: "controller"
-    })
-    .when("/home", {
-        templateUrl: "views/home.html",
-        controller: "controller"
-    })
-    .when("/nota-disciplina", {
-        templateUrl: "views/nota-disciplina.html",
-        controller: "controller"
-    })
-    .otherwise ({
-        templateUrl: "views/login.html",
-        controller: "controller"
-     });
+  $routeProvider
+  .when("/notas-frequencias", {
+    templateUrl: "views/notas-frequencias.html",
+    controller: "controller"
+  })
+  .when("/login", {
+    templateUrl: "views/login.html",
+    controller: "controller"
+  })
+  .when("/home", {
+    templateUrl: "views/home.html",
+    controller: "controller"
+  })
+  .when("/nota-disciplina", {
+    templateUrl: "views/nota-disciplina.html",
+    controller: "controller"
+  })
+  .otherwise ({
+    templateUrl: "views/login.html",
+    controller: "controller"
+   });
 });
--- public/js/diretives/navbar.js
+++ public/js/diretives/navbar.js
@@ -1,10 +1,10 @@
 angular.module("app").directive("navbar", function() {
-    return {
-        templateUrl: "views/navbar.html",
-        replace: false,
-        restrict: "E",
-        scope: {
-            title: '@'
-        }
+  return {
+    templateUrl: "views/navbar.html",
+    replace: false,
+    restrict: "E",
+    scope: {
+      title: '@'
     }
+  }
 })
