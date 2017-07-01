a32cea1c46c6c09a57fabafbc628d110bb92decd
Renan Dias <renan@jera.com.br>
2017-07-01 13:46:04 -0400

    Transfering code to this repository

---
+++ public/js/diretives/navbar.js
@@ -0,0 +1,10 @@
+angular.module("app").directive("navbar", function() {
+    return {
+        templateUrl: "views/navbar.html",
+        replace: false,
+        restrict: "E",
+        scope: {
+            title: '@'
+        }
+    }
+})
---
+++ server.js
@@ -0,0 +1,9 @@
+// alurapic/sever.js
+
+var http = require('http');
+var app = require('./config/express');
+
+http.createServer(app)
+.listen(process.env.PORT || 3000, function() {
+    console.log('Server started');
+});
